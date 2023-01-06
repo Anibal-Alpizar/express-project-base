e41157a163561488399ac95f92d143cc4b74136b
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-05 22:38:15 -0600

    express simple server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => {
+  res.sendFile("./static/index.html", {
+    root: __dirname
+  });
+});
+
+app.listen(3000);
+console.log(`server on port ${3000}`);
