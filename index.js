86e9aeb79c50fe6dea131935fd957919012cf977
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-05 22:52:42 -0600

    routing

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 
 const app = express();
+
 app.get("/", (req, res) => {
-  res.sendFile("./static/index.html", {
-    root: __dirname
-  });
+  res.send("hello world");
+});
+
+app.get("/about", (req, res) => {
+  res.send("about");
+});
+
+app.get("/weather", (req, res) => {
+  res.send("The current weather is nice");
+});
+
+app.use((req, res) => {
+  res.status(404).send("Not found");
 });
 
 app.listen(3000);
+
 console.log(`server on port ${3000}`);
