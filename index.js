25a0c021a755a9cdc59bd772024c0aa828259ad9
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-06 15:26:57 -0600

    Request params

--- index.js
+++ index.js
@@ -6,10 +6,28 @@ app.use(express.text());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/user", (req, res) => {
-  console.log(req.body);
-  res.send("Nuevo usuario creado");
+app.get("/hello/:user", (req, res) => {
+  console.log(typeof req.params.user);
+  res.send(`hello ${req.params.user.toUpperCase()}`);
 });
+
+app.get("/add/:x/:y", (req, res) => {
+  const { x, y } = req.params;
+  res.send(`Result: ${parseInt(x) + parseInt(y)}`);
+});
+
+app.get("/users/:username/photo", (req, res) => {
+  if (req.params.username === "anibal") {
+    return res.sendFile("./Javascript.png", {
+      root: __dirname,
+    });
+  }
+  res.send("el usuario no tiene acceso");
+});
+
+app.get('/name/:name/age/:age', (req,res)=>{
+  res.send(`El usuario: ${req.params.name}, tiene ${req.params.age} de edad`)
+})
 
 app.listen(3000);
 console.log(`server on port ${3000}`);
