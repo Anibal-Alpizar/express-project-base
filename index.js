cbbc5db292515a80d0f8a7a6e9e235fbe4cc437f
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-06 23:04:16 -0600

    introduction to middlewares

--- index.js
+++ index.js
@@ -6,13 +6,27 @@ app.use(express.text());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/profile/:user", (req, res) => {
+  res.send(`Hello, ${req.params.user}!`);
+});
+
+// middleware 1
 app.use((req, res, next) => {
-  console.log(`Route: ${req.url}, method: ${req.method}`)
+  console.log(`Route: ${req.url}, method: ${req.method}`);
   next();
 });
 
-app.get("/profile", (req, res) => {
-  res.send("profile page");
+// middleware 2
+app.use((req, res, next) => {
+  if (req.query.login === "anibal123") {
+    next();
+  } else {
+    res.send("no autorizado");
+  }
+});
+
+app.get("/dashboard", (req, res) => {
+  res.send("dashboard page");
 });
 
 app.listen(3000);
