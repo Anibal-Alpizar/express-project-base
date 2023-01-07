ae5643c25178306e76893408de7847ab90c4ff89
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-07 17:57:21 -0600

    Static Files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 
 //settings
 app.set("port", 3000);
-app.set('case sensitive routing', true);
 
 //middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-app.get("/UserName", (req, res) => {
-  res.send("username route");
+app.get("/", (req, res) => {
+  res.send("home");
 });
+
+console.log(__dirname);
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.listen(app.get("port"));
 console.log(`server ${app.get("appName")} on port ${app.get("port")}`);
